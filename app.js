c4c7c4dfaf939e15685fb09b139e0b49a9fb08d2
Ahmed Hassan <ahmedhassan.135@gmail.com>
2023-09-28 21:23:34 +0500

    Uncommented critical code previously commented for testing purposes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const session = require("express-session");
 const config = require("config");
 
-//const passport = require("./config/passportConfig");
+const passport = require("./config/passportConfig");
 
 const app = express();
 require("dotenv").config();
@@ -25,27 +25,27 @@ function normalizePort(val) {
 }
 const port = normalizePort(process.env.PORT || 3000);
 
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-// app.use(
-//   session({
-//     secret: "secret",
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// const authRouter = require("./routes/authRoutes");
-// const containerRouter = require("./routes/containerRoutes");
-// const blobRouter = require("./routes/blobRoutes");
+const authRouter = require("./routes/authRoutes");
+const containerRouter = require("./routes/containerRoutes");
+const blobRouter = require("./routes/blobRoutes");
 
-// app.use("/auth", authRouter);
-// app.use("/container", containerRouter);
-// app.use("/blob", blobRouter);
+app.use("/auth", authRouter);
+app.use("/container", containerRouter);
+app.use("/blob", blobRouter);
 
 app.get("/", (req, res) => {
   console.log();
